a9302529a95e8be7407c27bd5a07064b0d6ab9cc
K Lauer <klauer@slac.stanford.edu>
2019-06-18 10:09:07 -0700

    ENH: add make_ioc_from_project script

---
+++ make_ioc_from_project.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+PROJECT_PATH=$1
+
+if [ -z "$(which tcparse-stcmd)" ]; then
+    echo "Unable to find tcparse-stcmd in your current environment." > /dev/stderr
+    exit 1
+fi
+
+if [ -z "$(which makeBaseApp.pl)" ]; then
+    echo "Unable to find makeBaseApp.pl in your current environment." > /dev/stderr
+    exit 1
+fi
+
+if [ -z "$PROJECT_PATH" ]; then
+    echo "Usage: $0 {path/to/project_name.tsproj} [ioc_name]" > /dev/stderr
+    echo "ioc_name defaults to ioc-{project_name}." > /dev/stderr
+    exit 1
+fi
+
+if [ ! -f "$PROJECT_PATH" ]; then
+    echo "$PROJECT_PATH does not exist." > /dev/stderr
+    exit 1
+fi
+
+PROJECT_BASENAME=$(basename -- "$PROJECT_PATH")
+PROJECT_EXTENSION="${PROJECT_BASENAME##*.}"
+PROJECT_FILENAME="${PROJECT_BASENAME%.*}"
+
+IOC_NAME=$2
+
+if [ -z "$IOC_NAME" ]; then
+    IOC_NAME="ioc-${PROJECT_FILENAME}"
+fi
+
+IOC_PATH="iocBoot/$IOC_NAME"
+
+echo "IOC name: $IOC_NAME"
+echo "IOC path: $IOC_PATH"
+
+if [ ! -d "$IOC_PATH" ]; then
+    echo "IOC directory does not exist; running makeBaseApp..."
+    makeBaseApp.pl -t ioc -a $(epicsHostArch.pl) -i $IOC_NAME
+fi
+
+pushd $IOC_PATH
+echo "Generating the st.cmd:"
+tcparse-stcmd -b adsMotion "$PROJECT_PATH" > st.cmd
+popd
